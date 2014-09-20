cabcf6a72c4cfbfefe28e276348dbe456312c27e
Alex <alexanderkonkel@gmail.com>
2014-09-19 22:41:17 -0500

    finished through part 2

--- run_analysis.R
+++ run_analysis.R
@@ -18,6 +18,11 @@ names(alldata)=datanames[,2]
 #subject numbers are in the subject files; we might want those
 test_sub=read.table("test/subject_test.txt")
 train_sub=read.table("train/subject_train.txt")
-subnum=c(test_sub,train_sub)
+subnum=rbind(test_sub,train_sub)
 alldata$subnum=subnum
 
+#on to step 2: pull out only the mean and std variables
+mean_val=grep("-mean()",datanames[,2],fixed=TRUE)
+std_val=grep("-std()",datanames[,2],fixed=TRUE)
+keep_val=c(mean_val,std_val)
+alldata2=alldata[,keep_val]
